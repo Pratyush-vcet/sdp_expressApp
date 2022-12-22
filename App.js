dabd7ad4bfdb63827394e4a73c4a36cbc4d52598
unknown <pratyush.202044105@vcet.edu.in>
2022-12-22 10:42:43 +0530

    1st middleware added

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 const express = require("express")
+const logger = require("./middlewares/logger")
 
 const app = express()
+app.use(logger)
 
-app.get("/greetings", (req,res) => {
+app.get("/greetings", (req, res) => {
     return res.status(200).json({
-        message:"Hello express project"
+        message: "Hello express project"
     })
 })
 
-app.listen(8000 , (error) => {
-    if(error) {
+app.listen(8000, (error) => {
+    if (error) {
         console.log("error", error)
     }
     console.log("Server running on 8000")
