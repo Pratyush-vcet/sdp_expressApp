768021e14c63915f44c95f2c89ac2dc2fa13efff
unknown <pratyush.202044105@vcet.edu.in>
2022-12-22 11:52:32 +0530

    moving towards adding mongooose

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
-
-
-const authRouter = require("./routes/authentication")
-const express = require("express")
-const logger = require("./middlewares/logger")
-const dotenv = require("dotenv")
-dotenv.config();
+const authRouter = require('./routes/authentication')
+const express = require('express')
+const logger = require('./middlewares/logger')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
 app.use(logger)
 
-app.get("/greetings", (req, res) => {
-    return res.status(200).json({
-        message: "Hello express project"
-    })
+app.get('/greetings', (req, res) => {
+  return res.status(200).json({
+    message: 'Hello express project'
+  })
 })
 
-app.use("/api/auth",authRouter)
+app.use('/api/auth', authRouter)
 
 app.listen(process.env.PORT, (error) => {
-    if (error) {
-        console.log("error", error)
-    }
-    console.log("Server running on " + process.env.PORT)
+  if (error) {
+    console.log('error', error)
+  }
+  console.log('Server running on ' + process.env.PORT)
 })
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,21 +1,21 @@
-const { Router } = require("express")
+const { Router } = require('express')
 
 const authRouter = Router()
 
-authRouter.post("/login", (req, res,) => {
-    return res.status(200).json({
-        message:"login successful"
-    })
+authRouter.post('/login', (req, res) => {
+  return res.status(200).json({
+    message: 'login successful'
+  })
 })
-authRouter.post("/register", (req, res,) => {
-    return res.status(200).json({
-        message:"registered successful"
-    })
+authRouter.post('/register', (req, res) => {
+  return res.status(200).json({
+    message: 'registered successful'
+  })
 })
-authRouter.post("/verify", (req, res,) => {
-    return res.status(200).json({
-        message:"email verified successful"
-    })
+authRouter.post('/verify', (req, res) => {
+  return res.status(200).json({
+    message: 'email verified successful'
+  })
 })
 
 module.exports = authRouter
